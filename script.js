edc84bb4e80a27e78c65d6d1ee9fb57d01db1191
capybara <136744912+B11156029@users.noreply.github.com>
2025-05-07 00:52:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const token = "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"; // ⛔ 請替換為你自己的 GitHub Token
+const token = "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"; // ⛔ 請替換為你自己的 GitHub Token
 const owner = "B11156029";
 const repo = "Mart";
 const path = "main.json";
